eef8c70d78dd9b16297e495c068e35b8318376a4
Jin Hui <JHjinhui990@outlook.com>
2025-08-08 14:18:59 +0800

    feat:基础服务初始化、集成swagger

---
+++ configs/config.go
@@ -0,0 +1,15 @@
+package configs
+
+import (
+	"github.com/spf13/viper"
+)
+
+func InitConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	return viper.ReadInConfig()
+}
